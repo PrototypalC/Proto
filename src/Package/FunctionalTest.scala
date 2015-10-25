08844b2e76f2a2580ca866bee08df96e0277fe8c
JohnReedLOL <johnreedlol@ufl.edu>
2015-10-25 11:14:30 -0400

    Now have immutable classes with compiler inferred subclasses. Also have implicit keyword for adapter pattern. Note that case classes really aren't designed for full OOP functionality - just for type matching / switch statements.

--- src/Package/FunctionalTest.scala
+++ src/Package/FunctionalTest.scala
@@ -1,5 +1,7 @@
 package Package
 // ExecutionContext is like a thread pool/executor. Promise is for callback on future value.
+
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.{ ExecutionContext, Promise }
 import scala.concurrent._
 // Success/Failure for futures
@@ -73,7 +75,76 @@ object FunctionalTest {
     }
     */
 
+    // Trait is the same as a Java 8 interface.
+    sealed trait Z { def minus: String }
+    // case class is 100% immutable and can only be modified via copy.
+    sealed abstract class A(val number: Int) extends Z { def minus = "a" }
+    // sealed is a bad keyword meaning "compiler, assume all super/subclasses are in same file."
+    sealed case class B(val number: Double) extends Z { def minus = "b" }
+    // sealed allows compiler to make assumptions/inferences about possible subclasses.
+    sealed case class C(override val number: Int = 6) extends A(number) {
+        override def minus = "override"
+    }
+
+    trait Monoid[T] {
+        def baseCase: T
+        def append(a: T, b: T): T
+    }
+    // ^ This is a monoid representing things that are addable & associative w/ base case.^^
+
+    // Subclass the monoid ("implicit" means using the adapter pattern.)
+    implicit object IntMonoid extends Monoid[Int] {
+        def baseCase = 0
+        def append(a: Int, b: Int) = a + b
+    }
+    /* // This produces ambiguity because it has the same type as IntMonoid
+    implicit object MultiplyMonoid extends Monoid[Int] {
+        def baseCase = 1
+        def append(a: Int, b: Int) = a * b
+    }*/
+
+    // String are also addable...
+    implicit object StringMonoid extends Monoid[String] {
+        def baseCase = ""
+        def append(a: String, b: String) = a + b
+    }
+
+    // Everything that goes in this function is adapted to interface "Addable"
+    // Each element in the list is adapted to the Addable[T] interface.
+    def sum[T](listOfAdaptables: List[T])(implicit addable: Monoid[T]) =
+        listOfAdaptables.foldLeft(addable.baseCase)(addable.append)
+
     def main(args: Array[String]) {
+
+        val list123 = List(1,2,3) // Note for performance reasons this should be mutable ListBuffer.
+        val sum123 = sum(list123)
+        println("Sum of elements 1,2,3 is: " + sum123) // "6"
+        val listXYZ = List("X", "Y", "Z")
+        val sumXYZ = sum(listXYZ)
+        println("Sum of elements X,Y,Z is: " + sumXYZ) // "XYZ"
+
+        //val immutableA = new A(5) // Case classes have no inheritance.
+        val immutableB = new B(2.5)
+       // println("A minus: " + immutableA.minus)
+        println("B minus: " + immutableB.minus)
+        println("B val: " + immutableB.number)
+        val immutableC = new C()
+        println("C minus: " + immutableC.minus)
+        println("C val: " + immutableC.number)
+        val bCopy = immutableB.copy(number = 77.6)
+        println("B copy: " + bCopy.number + "\n")
+
+        def printTerm(term: Z) {
+            term match {
+                case B(number: Double) =>
+                    println("Hello B: " + number)
+                case C(number: Int) =>
+                    println("Hello C: " + number)
+            }
+        }
+        printTerm(immutableC) // 6
+        printTerm(bCopy) // 77.6
+
         println("Added 5 and 6 to get: " + add1(5)(6))
         println("Added 5 and 6 to get: " + add2(5)(6))
         val halfAddition = add1(5)
